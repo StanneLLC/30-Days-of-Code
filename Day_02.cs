6eccadcaee95691527a76e9a56a94fe0699abd65
stanne <stanne@users.noreply.github.com>
2017-11-12 18:59:27 -0500

    Update Day2 and submit Day3

--- Day_02.cs
+++ Day_02.cs
@@ -47,3 +47,22 @@ We round  to the nearest dollar (integer) and then print our result:
 The total meal cost is 15 dollars.
 
 */
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+class Solution {
+    static void Main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+		var mealCost = double.Parse(Console.ReadLine());
+        var tipPercent = int.Parse(Console.ReadLine());
+        var taxPercent = int.Parse(Console.ReadLine());
+        
+        var tip = mealCost * tipPercent / 100;
+        var tax = mealCost * taxPercent / 100;
+        var totalCost = mealCost + tip + tax;
+        
+        Console.WriteLine($"The total meal cost is {totalCost} dollars.");
+    }
+}
+
--- Day_03.cs
+++ Day_03.cs
@@ -1,18 +1,67 @@
+//Day 3: Intro to Conditional Statements
+/*
+Objective 
+In this challenge, we're getting started with conditional statements. Check out the Tutorial tab for learning materials and an instructional video!
+
+Task 
+Given an integer, , perform the following conditional actions:
+
+If  is odd, print Weird
+If  is even and in the inclusive range of  to , print Not Weird
+If  is even and in the inclusive range of  to , print Weird
+If  is even and greater than , print Not Weird
+Complete the stub code provided in your editor to print whether or not  is weird.
+
+Input Format
+
+A single line containing a positive integer, .
+
+Constraints
+
+Output Format
+
+Print Weird if the number is weird; otherwise, print Not Weird.
+
+Sample Input 0
+
+3
+Sample Output 0
+
+Weird
+Sample Input 1
+
+24
+Sample Output 1
+
+Not Weird
+Explanation
+
+Sample Case 0:  
+ is odd and odd numbers are weird, so we print Weird.
+
+Sample Case 1:  
+ and  is even, so it isn't weird. Thus, we print Not Weird.
+ */
+ 
+ 
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 class Solution {
+
     static void Main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+        int N = Convert.ToInt32(Console.ReadLine());
         
-        var mealCost = double.Parse(Console.ReadLine());
-        var tipPercent = int.Parse(Console.ReadLine());
-        var taxPercent = int.Parse(Console.ReadLine());
+        if(N % 2 != 0 || (N % 2 == 0 && (N > 5 && N < 21)))
+        {
+            Console.WriteLine("Weird");
+        }
         
-        var tip = mealCost * tipPercent / 100;
-        var tax = mealCost * taxPercent / 100;
-        var totalCost = mealCost + tip + tax;
-        
-        Console.WriteLine($"The total meal cost is {totalCost} dollars.");
+        else if(N % 2 == 0 && N > 20 || (N % 2 == 0 && (N > 1 && N < 6)))
+        {
+            Console.WriteLine("Not Weird");
+        }
+            
+        }
     }
-}
